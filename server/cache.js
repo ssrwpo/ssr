5ebcefdff685991bafb44413f2674772f5e8ce63
PEM-- <pemarchandet@gmail.com>
2017-01-16 13:28:50 +0100

    Add logs on caching

--- server/cache.js
+++ server/cache.js
@@ -1,4 +1,5 @@
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
+import logger from './logger';
 
 checkNpmVersions({
   receptacle: '1.x',
@@ -13,12 +14,15 @@ class Cache {
     this.receptacle = new Receptacle({ max: Cache.MAX_ITEMS });
   }
   setPage(url, head, body) {
+    logger.debug('Cache Page', url);
     this.receptacle.set(url, { type: 200, head, body }, { ttl: Cache.DEFAULT_TTL });
   }
   setRedirect(url, location) {
+    logger.debug('Cache Redirect', url);
     this.receptacle.set(url, { type: 301, location }, { ttl: Cache.DEFAULT_TTL });
   }
   setNotFound(url) {
+    logger.debug('Cache NotFound', url);
     this.receptacle.set(url, { type: 404 }, { ttl: Cache.DEFAULT_TTL });
   }
   has(url) {
