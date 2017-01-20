b1f200d0cbb9aeef376bb0f3a2f36615b629c320
PEM-- <pemarchandet@gmail.com>
2017-01-20 14:01:50 +0100

    Publish version 0.0.11

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 const name = 'ssrwpo:ssr';
 Package.describe({
   name,
-  version: '0.0.10',
+  version: '0.0.11',
   summary: 'SSR - Router with SSR for Node & Meteor',
   git: 'https://github.com/ssr-server/ssr',
   documentation: 'README.md',
