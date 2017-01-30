4a8e2b7f7580fe54b0e5a5d8b6e1cd108d914435
PEM-- <pemarchandet@gmail.com>
2017-01-30 19:49:15 +0100

    Publish 1.9.2

--- server/utils/peerDependencies.js
+++ server/utils/peerDependencies.js
@@ -5,6 +5,7 @@ checkNpmVersions({
   express: '4.x',
   helmet: '3.x',
   i18next: '6.0.x',
+  'i18next-express-middleware': '1.x',
   'i18next-node-remote-backend': '0.0.x',
   'i18next-xhr-backend': '1.3.x',
   lodash: '4.x',
@@ -16,6 +17,7 @@ checkNpmVersions({
   'react-redux': '5.x',
   receptacle: '1.x',
   redux: '3.x',
+  'serialize-javascript': '1.x',
   useragent: '2.1.x',
   winston: '2.x',
 }, 'ssrwpo:ssr');
