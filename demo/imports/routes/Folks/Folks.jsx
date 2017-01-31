b455b71f973b3ad76df9001300e63549e92dd19b
PEM-- <pemarchandet@gmail.com>
2017-01-31 14:55:31 +0100

    Adapt Folks

--- demo/imports/routes/Folks/Folks.jsx
+++ demo/imports/routes/Folks/Folks.jsx
@@ -1,38 +1,46 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes as pt } from 'react';
+import querystring from 'querystring';
 import Helmet from 'react-helmet';
 import { pure } from 'meteor/ssrwpo:ssr';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import Folk from './routes/Folk';
 
 const Nav = ({ folks }) => (
   <nav><ul>
     {folks.map(folk => <li key={folk.id}>
-      <Link to={{ query: { folkId: folk.id } }}>{folk.name}</Link>
+      <Link to={`/folks?folkId=${folk.id}`}>{folk.name}</Link>
     </li>)}
   </ul></nav>
 );
 Nav.propTypes = {
-  folks: PropTypes.array.isRequired,
+  folks: pt.array.isRequired,
 };
 
-const Folks = ({ folks, location }) => {
-  const folkId = location.query && location.query.folkId;
-  const folk = folkId && folks.find(item => item.id === folkId);
+const Folks = ({ folks, match, location }) => {
+  const query = location && location.search
+    ? querystring.parse(location.search.slice(1))
+    : null;
+  const folk = query && query.folkId
+    ? folks.find(item => item.id === query.folkId)
+    : null;
   return (
     <div>
       <Helmet title="Folks" />
       <h2>Folks</h2>
-      {
-        folk
-        ? <Folk name={folk.name} />
-        : <Nav folks={folks} />
-      }
+      <Route
+        exact path={match.url} render={() => (
+          folk
+          ? <Folk name={folk.name} />
+          : <Nav folks={folks} />
+        )}
+      />
     </div>
   );
 };
 Folks.propTypes = {
-  folks: PropTypes.array.isRequired,
-  location: PropTypes.object.isRequired,
+  folks: pt.array.isRequired,
+  match: pt.object.isRequired,
+  location: pt.object.isRequired,
 };
 export default connect(state => ({ folks: state.Folks }))(pure(Folks));
