41ef4d1448030e79dbab7417be215e116d86e53d
gbhrdt <marius.gebhardt@gmail.com>
2017-02-28 14:21:02 +0100

    fix indentation

--- demo/imports/i18n/i18nClient.js
+++ demo/imports/i18n/i18nClient.js
@@ -4,16 +4,16 @@ import XHR from 'i18next-xhr-backend';
 i18n
   .use(XHR)
   .init({
-      whitelist: ['en', 'tr', 'fr'],
-      fallbackLng: 'en',
-      debug: false,
-      // have a common namespace used around the full app
-      ns: ['common', 'greetings'],
-      defaultNS: 'common',
+    whitelist: ['en', 'tr', 'fr'],
+    fallbackLng: 'en',
+    debug: false,
+    // have a common namespace used around the full app
+    ns: ['common', 'greetings'],
+    defaultNS: 'common',
 
-      backend: {
-          loadPath: `${Meteor.absoluteUrl()}locales/{{lng}}/{{ns}}.json`,
-      },
+    backend: {
+      loadPath: `${Meteor.absoluteUrl()}locales/{{lng}}/{{ns}}.json`,
+    },
   });
 
 export default i18n;
