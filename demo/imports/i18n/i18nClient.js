3a7846568547c6e0e845649668f247b5aadeb9d9
gbhrdt <marius.gebhardt@gmail.com>
2017-02-28 14:19:57 +0100

    add ' i18n-xhr-backend' to the demo

--- demo/imports/i18n/i18nClient.js
+++ demo/imports/i18n/i18nClient.js
@@ -1,13 +1,19 @@
 import i18n from 'i18next';
+import XHR from 'i18next-xhr-backend';
 
 i18n
+  .use(XHR)
   .init({
-    whitelist: ['en', 'tr', 'fr'],
-    fallbackLng: 'en',
-    debug: false,
-    // have a common namespace used around the full app
-    ns: ['common', 'greetings'],
-    defaultNS: 'common',
+      whitelist: ['en', 'tr', 'fr'],
+      fallbackLng: 'en',
+      debug: false,
+      // have a common namespace used around the full app
+      ns: ['common', 'greetings'],
+      defaultNS: 'common',
+
+      backend: {
+          loadPath: `${Meteor.absoluteUrl()}locales/{{lng}}/{{ns}}.json`,
+      },
   });
 
 export default i18n;
