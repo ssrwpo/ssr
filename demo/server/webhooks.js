19cf2256d5bb36686fa1ef9c93c67a4791480efd
PEM-- <pemarchandet@gmail.com>
2017-03-09 08:02:48 +0100

    Package bumped

--- demo/server/webhooks.js
+++ demo/server/webhooks.js
@@ -3,10 +3,11 @@ import { logger } from 'meteor/ssrwpo:ssr';
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+// For more details see: https://github.com/expressjs/body-parser#express-route-specific
 const webhooks = (app) => {
   // eslint-disable-next-line no-unused-vars
   app.post('/api/ready', urlencodedParser, (req, res, next) => {
-    logger.info('Called with', req.body);
+    logger.info('Called with:', req.body);
     res.set('Content-Type', 'application/json');
     res.end(JSON.stringify({ ready: true }));
   });
