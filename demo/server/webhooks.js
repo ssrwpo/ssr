7c6f4de505a4ebf34d315cfa7b6d0f159a6c5875
PEM-- <pemarchandet@gmail.com>
2017-03-09 08:09:29 +0100

    Fix #41

--- demo/server/webhooks.js
+++ demo/server/webhooks.js
@@ -1,12 +1,12 @@
 import bodyParser from 'body-parser';
 import { logger } from 'meteor/ssrwpo:ssr';
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const jsonParser = bodyParser.json();
 
 // For more details see: https://github.com/expressjs/body-parser#express-route-specific
 const webhooks = (app) => {
   // eslint-disable-next-line no-unused-vars
-  app.post('/api/ready', urlencodedParser, (req, res, next) => {
+  app.post('/api/ready', jsonParser, (req, res) => {
     logger.info('Called with:', req.body);
     res.set('Content-Type', 'application/json');
     res.end(JSON.stringify({ ready: true }));
