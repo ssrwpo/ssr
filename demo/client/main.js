b84664cd3a5db37d525124c9145f82d69c1a1f72
PEM-- <pemarchandet@gmail.com>
2017-03-24 13:22:14 +0100

    Adapt demo to redux-logger 3.x

--- demo/client/main.js
+++ demo/client/main.js
@@ -1,6 +1,6 @@
 import { createRouter, logger, getStore } from 'meteor/ssrwpo:ssr';
 import * as appReducers from '/imports/reducers';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise';
 import MainApp from '/imports/app/MainApp';
