2a28ccf7b2b5f02c34cab35b1b113f6a19183f8b
PEM-- <pemarchandet@gmail.com>
2017-04-03 17:10:28 +0200

    First atom: Button

---
+++ demo/imports/styles/transitions.js
@@ -0,0 +1,4 @@
+const defaultTransition = '0.3s ease-in-out';
+
+// eslint-disable-next-line import/prefer-default-export
+export { defaultTransition };
--- demo/imports/ui/atom/Button.jsx
+++ demo/imports/ui/atom/Button.jsx
@@ -1,12 +1,19 @@
 import styled from 'styled-components';
 import { bgColor, secondaryColor } from '/imports/styles/colors';
+import { defaultTransition } from '/imports/styles/transitions';
 
 export default styled.button`
   background: ${secondaryColor};
-  border: 0;
+  border: 2px solid ${secondaryColor};
+  border-radius: 2px;
   color: ${bgColor};
   padding: 0.3em 0.5em;
   font-size: 1.1em;
   font-weight: bold;
   outline: 0;
+  transition: color ${defaultTransition}, background-color ${defaultTransition};
+  &:hover {
+    background-color: ${bgColor};
+    color: ${secondaryColor};
+  }
 `;
